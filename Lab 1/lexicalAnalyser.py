a6cf5c6abf9301bcc0e116777669652552cbf934
Shushrut Kumar <60442063+shushrutsharma@users.noreply.github.com>
2021-01-27 11:12:26 +0530

    python version added

---
+++ Lab 1/lexicalAnalyser.py
@@ -0,0 +1,26 @@
+file  = open("add.c", 'r')
+lines = file.readlines()
+
+keywords    = ["void", "main", "int", "float", "bool", "if", "for", "else", "while", "char", "return"]
+operators   = ["=", "==", "+", "-", "*", "/", "++", "--", "+=", "-=", "!=", "||", "&&"]
+punctuations= [";", "(", ")", "{", "}", "[", "]"]
+
+def is_int(x):
+    try:
+        int(x)
+        return True
+    except:
+        return False
+
+for line in lines:
+    for i in line.strip().split(" "):
+        if i in keywords:
+            print (i, " is a keyword")
+        elif i in operators:
+            print (i, " is an operator")
+        elif i in punctuations:
+            print (i, " is a punctuation")
+        elif is_int(i):
+            print (i, " is a number")
+        else:
+            print (i, " is an identifier")
