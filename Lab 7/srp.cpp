eb982d86ec404a4ab15757405ffadca9a3148b26
Shushrut Kumar <60442063+shushrutsharma@users.noreply.github.com>
2021-03-23 20:37:28 +0530

    lab 7 added

--- Lab 7/srp.cpp
+++ Lab 7/srp.cpp
@@ -1,110 +1,64 @@
-#include <bits/stdc++.h> 
-using namespace std; 
-int z = 0, i = 0, j = 0, c = 0; 
-char a[16], ac[20], stk[15], act[10]; 
-void check() 
-{ 
-	strcpy(ac,"REDUCE TO E -> "); 
-
-	for(z = 0; z < c; z++) 
-	{ 
-		// checking for producing rule E->4 
-		if(stk[z] == '4') 
-		{ 
-			printf("%s4", ac); 
-			stk[z] = 'E'; 
-			stk[z + 1] = '\0'; 
-				
-			//pinting action 
-			printf("\n$%s\t%s$\t", stk, a); 
-		} 
-	} 
-			
-	for(z = 0; z < c - 2; z++) 
-	{ 
-		// checking for another production 
-		if(stk[z] == '2' && stk[z + 1] == 'E' && 
-								stk[z + 2] == '2') 
-		{ 
-			printf("%s2E2", ac); 
-			stk[z] = 'E'; 
-			stk[z + 1] = '\0'; 
-			stk[z + 2] = '\0'; 
-			printf("\n$%s\t%s$\t", stk, a); 
-			i = i - 2; 
-		} 
-			
-	} 
-			
-	for(z = 0; z < c - 2; z++) 
-	{ 
-		//checking for E->3E3 
-		if(stk[z] == '3' && stk[z + 1] == 'E' && 
-								stk[z + 2] == '3') 
-		{ 
-			printf("%s3E3", ac); 
-			stk[z]='E'; 
-			stk[z + 1]='\0'; 
-			stk[z + 1]='\0'; 
-			printf("\n$%s\t%s$\t", stk, a); 
-			i = i - 2; 
-		} 
-	} 
-	return ; // return to main 
-} 
-	
-// Driver Function 
-int main() 
-{ 
-	printf("GRAMMAR is -\nE->2E2 \nE->3E3 \nE->4\n");	 
-		
-	// a is input string 
-	strcpy(a,"32423"); 
-		
-	// strlen(a) will return the length of a to c 
-	c=strlen(a); 
-		
-	// "SHIFT" is copied to act to be printed 
-	strcpy(act,"SHIFT"); 
-		
-	// This will print Lables (column name) 
-	printf("\nstack \t input \t action"); 
-		
-	// This will print the initial 
-	// values of stack and input 
-	printf("\n$\t%s$\t", a); 
-		
-	// This will Run upto length of input string 
-	for(i = 0; j < c; i++, j++) 
-	{ 
-		// Printing action 
-		printf("%s", act); 
-			
-		// Pushing into stack 
-		stk[i] = a[j];	 
-		stk[i + 1] = '\0'; 
-			
-		// Moving the pointer 
-		a[j]=' '; 
-			
-		// Printing action 
-		printf("\n$%s\t%s$\t", stk, a); 
-			
-		// Call check function ..which will 
-		// check the stack whether its contain 
-		// any production or not 
-		check(); 
-	} 
-		
-	// Rechecking last time if contain 
-	// any valid production then it will 
-	// replace otherwise invalid 
-	check(); 
-		
-	// if top of the stack is E(starting symbol) 
-	// then it will accept the input 
-	if(stk[0] == 'E' && stk[1] == '\0') 
-		printf("Accept\n"); 
-	else //else reject 
-		printf("Reject\n"); 
-} 
+#include<iostream>
+#include<string.h>
+using namespace std;
+struct prodn
+{
+	char p1[10];
+	char p2[10];
+};
+int main()
+{
+	char input[20],stack[50],temp[50],ch[2],*t1,*t2,*t;
+	int i,j,s1,s2,s,count=0;
+	struct prodn p[10];
+	FILE *fp=fopen("sr_input.txt","r");
+	stack[0]='\0';
+	cout<<"Enter the Input String:\n";
+	cin>>input;
+	while(!feof(fp))
+	{
+		fscanf(fp,"%s\n",temp);
+		t1=strtok(temp,"->");
+		t2=strtok(NULL,"->");
+		strcpy(p[count].p1,t1);
+		strcpy(p[count].p2,t2);
+		count++;
+	}
+	i=0;
+	while(1)
+	{
+		if(i<strlen(input))
+		{
+			ch[0]=input[i];
+			ch[1]='\0';
+			i++;
+			strcat(stack,ch);
+			cout<<"\n"<<stack;
+		}
+		for(j=0;j<count;j++)
+		{
+			t=strstr(stack,p[j].p2);
+			if(t!=NULL)
+			{
+				s1=strlen(stack);
+				s2=strlen(t);
+				s=s1-s2;
+				stack[s]='\0';
+				strcat(stack,p[j].p1);
+				cout<<"\n"<<stack;
+				j=-1;
+			}
+		}
+		if(strcmp(stack,"E")==0&&i==strlen(input))
+		{
+			cout<<"\n\nAccepted";
+			break;
+		}
+		if(i==strlen(input))
+		{
+			cout<<"\n\nNot Accepted";
+			break;
+		}
+	}
+	return 0;
+}
